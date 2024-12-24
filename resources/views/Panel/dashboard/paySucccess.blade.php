70e0d1babb6df3bbc5614dcb710ddd613262f913
Hammad-waseem-developer <107311660+MuhammadHammadWaseem@users.noreply.github.com>
2024-12-24 08:23:55 -0800

    Update paySucccess.blade.php

--- resources/views/Panel/dashboard/paySucccess.blade.php
+++ resources/views/Panel/dashboard/paySucccess.blade.php
@@ -1,5 +1,35 @@
-@extends('Panel.Layout.master')
-@section('content')
+{{-- @extends('Panel.Layout.master')
+@section('content') --}}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Click Invitation Dashboard</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="{{ asset('assets/images/favicon.png') }}" type="favicon.png" sizes="32x32">
+        <link rel="stylesheet" href="{{ asset('assets/css/lib.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/Panel/css/style.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet">
+        <link rel="stylesheet" href ="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+        <link rel="stylesheet" href="https://searchmarketingservices.online/fonts/index.css">
+    </head>
+
+<body class="custom_scroll_hide">
+    
+
     @php
         use App\Helpers\GeneralHelper;
         $currentEventId = GeneralHelper::getEventId();
@@ -39,7 +69,14 @@
         </div>
 
     </div>
-@endsection
+
+    <script>
+    console.log("Event Id" ,{{ $currentEventId }});
+    console.log("User Id" ,{{ $user->id }});
+    </script>
+</body>
+</html>
+{{-- @endsection
 
 @section('scripts')
-@endsection
+@endsection --}}
