90bec0e1402f947db19fd43c35fe3fe198b2202e
Hammad-waseem-developer <nasirahmedwaseemnasir@gmail.com>
2025-03-14 15:06:47 -0700

    CSS Set !

--- resources/views/Panel/dashboard/pay.blade.php
+++ resources/views/Panel/dashboard/pay.blade.php
@@ -307,7 +307,7 @@
             <div class="col-lg-12">
                 @if (session('error'))
                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>Error:</strong> {{ session('error') }}
+                        <strong>Note:</strong> {{ session('error') }}
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
